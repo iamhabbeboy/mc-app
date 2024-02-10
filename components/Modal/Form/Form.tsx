d7bd2034185764361875053d91a6afee10fc639b
Abiodun Azeez <iamhabbeboy@gmail.com>
2024-02-10 17:43:05 +0100

    Form modal

--- components/Modal/Form/Form.tsx
+++ components/Modal/Form/Form.tsx
@@ -56,8 +56,8 @@ const Form = () => {
     window.dispatchEvent(openModal);
   }
   return (
-    <>
-      <div className="mx-auto w-[80%]">
+    <div className="overflow-auto h-screen">
+      <div className="mx-auto lg:w-[80%] w-full">
         <h1 className="modal__title text-center"> Enter details </h1>
         <h2 className="mt-5 text-2xl text-center">Enter your name and precise address</h2>
         <div className="mt-10">
@@ -91,13 +91,13 @@ const Form = () => {
             <div> I understand that my provided information, including names and location, will be used solely for the purpose of generating personalized images for this Valentine&apos;s competition. I acknowledge that my data will be kept confidential and will not be shared with any third parties.</div>
           </div>
         </div>
-        <div className="mx-auto text-center">
+        <div className="mx-auto text-center mb-20">
           <button className="button button__create text-white py-2 px-7" onClick={handleCloseForm}>Proceed To Upload Image </button>
           <p className="mt-5">{" "}</p>
           <button className="button button__outline py-2 px-7" onClick={handleCloseAllModal}>Go Back </button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 export default Form;
