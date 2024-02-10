4a81e2b1ed751332cae1e2c61f3a7da03afa5eb4
Abiodun Azeez <iamhabbeboy@gmail.com>
2024-02-10 17:50:32 +0100

    Fix image uploader modal md

--- components/Modal/ImageUpload/ImageUpload.tsx
+++ components/Modal/ImageUpload/ImageUpload.tsx
@@ -40,13 +40,13 @@ const ImageUpload = () => {
         <button onClick={handleCloseImageUploadModal}><Image src="./logo.svg" width={154} height={34} alt="Logo image" /></button>
       </div>
       <div className="bg-[url('/pattern-2.png')] h-[300px] fixed bottom-0 w-full">
-        <div className="relative bottom-[400px] mx-auto w-[60%]">
+        <div className="relative bottom-[400px] mx-auto lg:w-[60%] w-full">
           <div className="text-center mt-10">
             <h1 className="section__title font-normal text-5xl inline"> Make L<Image src="/love.svg" width={42} height={33} alt="Love shot image" className="inline" />ve </h1>
             <h3 className="inline font-normal text-4xl section__subtext pl-5">Possible</h3>
           </div>
           <p className="text-center text-2xl">Upload a image of you and your partner</p>
-          <div className={`${style.imageupload__layout} mt-20 border-2 border-dashed p-10 text-center rounded-lg`}>
+          <div className={`${style.imageupload__layout} lg:mt-20 mt-5 border-2 border-dashed p-10 text-center rounded-lg`}>
             <div className="mx-auto" {...getRootProps()}>
             {/* <div className="mx-auto"> */}
               <div className="mx-auto w-[60px]">
